10f472c928ab060db98763500c8f34af355f6ef7
Chris Benka <chrisbenka@gmail.com>
2022-07-02 22:03:15 -0500

    dominion big money bot with duchy dancing

--- open_spiel/python/bots/dominion_bots.py
+++ open_spiel/python/bots/dominion_bots.py
@@ -1,46 +1,86 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
+from scipy import rand
 from open_spiel.python.games import dominion
 from operator import itemgetter
+import random 
 import numpy as np
 import pyspiel
 
 
 class BigMoneyBot(pyspiel.Bot):
-	def __init__(self, player_id, duchy_dancing = False):
+	def __init__(self, observer, player_id, duchy_dancing = False):
 		pyspiel.Bot.__init__(self)
+		self.observer = observer
 		self._duchy_dancing = duchy_dancing
 		self._player_id = player_id
 	
+	def player_id(self):
+		return self._player_id
+	
+	def provides_policy(self):
+		return True
+		
 	@staticmethod
 	def purchase_treasure_card_if_avail(card: dominion.TreasureCard, state):
 		return card.buy if state.supply_piles[card.name].qty > 0 else dominion.END_PHASE_ACTION
+	
+	def random_policy(self,legal_actions):
+		p = 1 / len(legal_actions)
+		action = random.choice(legal_actions)
+		policy = [(action, p) for action in legal_actions]
+		return policy, action
 
-	def step_with_policy(self,state: dominion.DominionGameState):
-		legal_actions = state.legal_actions()
+	def _is_penultimate_province(self):
+		return self.observer.dict['victory_piles'].tolist()[3] == 1
+	
+	def _duchy_available(self):
+		return self.observer.dict['victory_piles'].tolist()[2] > 0
+
+	def play_first_treasure_card(self):
+		obs_dict = self.observer.dict
+		unique_cards_in_hand = itemgetter(*np.nonzero(obs_dict['hand'])[0].tolist())(dominion._ALL_CARDS)
+		if isinstance(unique_cards_in_hand,tuple):
+			return next(filter(lambda card: isinstance(card, dominion.TreasureCard),unique_cards_in_hand)).play
+		else:
+			return unique_cards_in_hand.play
+		
+	def step_with_policy(self,state):
+		legal_actions = state._legal_actions(self._player_id)
+
+		if legal_actions == [dominion.END_PHASE_ACTION]:
+			policy = [(dominion.END_PHASE_ACTION,1)]
+			return policy, dominion.END_PHASE_ACTION
 		if not legal_actions:
 			return [], pyspiel.INVALID_ACTION
-		obs_dict = state.observation_dict()
-		turn_phase = obs_dict['TurnPhase'][0]
+		
+		self.observer.set_from(state,self.player_id())
+		obs_dict = self.observer.dict
+		turn_phase = dominion.TurnPhase(obs_dict['TurnPhase'].tolist()[0])
+		effect = obs_dict['effect'].tolist()[0]
+
+		if effect != 0:
+			return self.random_policy(legal_actions)
+
 		action = dominion.END_PHASE_ACTION
-		if turn_phase is not dominion.TurnPhase.TREASURE_PHASE and turn_phase is not dominion.TurnPhase.BUY_PHASE:
-			action = dominion.END_PHASE_ACTION
-		elif turn_phase is dominion.TurnPhase.TREASURE_PHASE:
-			unique_cards_in_hand = itemgetter(*np.nonzero(obs_dict['hand'])[0].tolist())(dominion._ALL_CARDS)
-			if type(unique_cards_in_hand) is tuple:
-				unique_cards_in_hand = list(unique_cards_in_hand)
-			else:
-				unique_cards_in_hand = [unique_cards_in_hand]
-			action = next(filter(lambda card: isinstance(card,dominion.TreasureCard),unique_cards_in_hand)).play
+		if turn_phase is dominion.TurnPhase.TREASURE_PHASE:
+			action = self.play_first_treasure_card()
 		elif turn_phase is dominion.TurnPhase.BUY_PHASE:
-			num_coins = obs_dict['coins'][0]
+			num_coins = obs_dict['coins'].tolist()[0]
 			if num_coins >= 3 and num_coins <= 5:
 				action = BigMoneyBot.purchase_treasure_card_if_avail(dominion.SILVER,state)
 			elif num_coins >= 6 and num_coins <= 7:
 				action = BigMoneyBot.purchase_treasure_card_if_avail(dominion.GOLD,state)
 			elif num_coins >= 8:
-				action = dominion.PROVINCE.buy
+				if self._duchy_dancing and self._is_penultimate_province() and self._duchy_available():
+					action = dominion.DUCHY.buy
+				else:
+					action = dominion.PROVINCE.buy
+		else:
+			return self.random_policy(legal_actions)
+		#deterministic policy
 		policy = [(legal_action,1) if legal_action is action else (legal_action,0) for legal_action in legal_actions]
 		return policy, action 
 	def step(self,state):
--- open_spiel/python/bots/dominion_bots_test.py
+++ open_spiel/python/bots/dominion_bots_test.py
@@ -1,5 +1,7 @@
+from xml import dom
 from absl.testing import absltest
 from absl import flags
+from collections import Counter 
 from open_spiel.python import games  # pylint: disable=unused-import
 from open_spiel.python.bots import dominion_bots 
 import numpy as np
@@ -7,146 +9,238 @@ import pyspiel
 from open_spiel.python.games import dominion
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string("game", "python_dominion", "Name of the game")
-flags.DEFINE_integer("num_players", 2, "Number of players")
-flags.DEFINE_string("kingdom_cards","Village, Laboratory, Festival, Market, Militia, Gardens, Chapel, Throne Room, Moneylender, Poacher", "names of 10 kingdom cards for gameplay")
+
+KINGDOM_CARDS = "Village,Laboratory,Festival,Market,Smithy,Militia,Gardens,Chapel,Witch,Workshop"
+NUM_PLAYERS = 2
+params = {'num_players': NUM_PLAYERS, 'kingdom_cards': KINGDOM_CARDS}
 
 
+def current_player_state(state):
+    return state.get_player(state._curr_player)
+
+def player_state(state,p_id):
+    return state.get_player(p_id)
+
+def player_has_cards(player,card_nms):
+    avail_counts = Counter(list(map(lambda card: card.name,player.draw_pile)))
+    request_card_counts = Counter(card_nms)
+    for name in request_card_counts:
+        if request_card_counts[name] > avail_counts[name]:
+            return False
+    return True
+
+def load_hand(state,card_nms):
+    player = current_player_state(state)
+    assert player.phase == dominion.TurnPhase.TREASURE_PHASE, "load_hand can only be called at start of player's turn"
+    assert len(card_nms) <= 5, "load_hand can only be called witb card_names <= 5"
+    player.draw_pile += player.hand
+    assert player_has_cards(player,card_nms), F"{card_nms} must be available for draw. Cards available for player's hand: {list(map(lambda card: card.name, player.draw_pile))}"
+    player.hand.clear()
+    cards = [state.supply_piles[name].card for name in card_nms]
+    for card in cards:
+        player.hand.append(card)
+        player.draw_pile.remove(card)
+    if player.has_action_cards:
+        player.phase = dominion.TurnPhase.ACTION_PHASE
 
 class BigMoneyBot(absltest.TestCase):
 
+    def test_is_penultimate_province(self):
+        game = pyspiel.load_game('python_dom',params)
+        observer = game.make_py_observer()
+        state = game.new_initial_state()
+        player_0_deck_order = [dominion.COPPER.id] * 3 + [dominion.ESTATE.id] * 2 + [dominion.COPPER.id] * 4 + [dominion.ESTATE.id]
+        player_1_deck_order = [dominion.COPPER.id] * 3 + [dominion.ESTATE.id] * 2 + [dominion.COPPER.id] * 4 + [dominion.ESTATE.id]
+        for a in player_0_deck_order:
+            state.apply_action(a)
+        for a in player_1_deck_order:
+            state.apply_action(a)
+        bot = dominion_bots.BigMoneyBot(observer=observer,player_id=0)
+        action = bot.step(state)
+        self.assertFalse(bot._is_penultimate_province())
+        state.supply_piles[dominion.PROVINCE.name].qty = 1
+        action = bot.step(state)
+        self.assertTrue(bot._is_penultimate_province()) 
+
     def test_buys_silver_with_3_coins(self):
-        game = pyspiel.load_game(FLAGS.game, {"num_players": FLAGS.num_players,"kingdom_cards": FLAGS.kingdom_cards})
+        game = pyspiel.load_game('python_dom',params)
+        observer = game.make_py_observer()
         bots = [
-            dominion_bots.BigMoneyBot(0),
-            dominion_bots.BigMoneyBot(1)
+            dominion_bots.BigMoneyBot(observer=observer,player_id=0),
+            dominion_bots.BigMoneyBot(observer=observer,player_id=1),
         ]
         state = game.new_initial_state()
-        #mock hand
-        state.load_hand(['Copper','Copper','Copper','Estate','Estate'])
-        actions = []
-        while state.current_player() is 0:
+        player_0_deck_order = [dominion.COPPER.id] * 3 + [dominion.ESTATE.id] * 2 + [dominion.COPPER.id] * 4 + [dominion.ESTATE.id]
+        player_1_deck_order = [dominion.COPPER.id] * 3 + [dominion.ESTATE.id] * 2 + [dominion.COPPER.id] * 4 + [dominion.ESTATE.id]
+        for a in player_0_deck_order:
+            state.apply_action(a)
+        for a in player_1_deck_order:
+            state.apply_action(a)
+        
+        actual_actions = []
+
+        while state.current_player() == 0:
             action = bots[0].step(state)
             state.apply_action(action)
-            actions.append(action)
+            actual_actions.append(action)
 
-        expected_actions = [dominion.COPPER.play] * 3 + [dominion.SILVER.buy]
-        np.testing.assert_array_equal(expected_actions,actions)
+        expected_actions = [dominion.COPPER.play] * 3  + [dominion.END_PHASE_ACTION] + [dominion.SILVER.buy] + [dominion.END_PHASE_ACTION]
+        np.testing.assert_array_equal(actual_actions,expected_actions)
     
     def test_buys_silver_with_4_coins(self):
-        game = pyspiel.load_game(FLAGS.game, {"num_players": FLAGS.num_players,"kingdom_cards": FLAGS.kingdom_cards})
+        game = pyspiel.load_game('python_dom',params)
+        observer = game.make_py_observer()
         bots = [
-            dominion_bots.BigMoneyBot(0),
-            dominion_bots.BigMoneyBot(1)
+            dominion_bots.BigMoneyBot(observer=observer,player_id=0),
+            dominion_bots.BigMoneyBot(observer=observer,player_id=1),
         ]
         state = game.new_initial_state()
-        #mock hand
-        state.load_hand(['Copper','Copper','Copper','Copper','Estate'])
-        actions = []
+        player_0_deck_order = [dominion.COPPER.id] * 4 + [dominion.ESTATE.id] * 1 + [dominion.COPPER.id] * 3 + [dominion.ESTATE.id] * 2
+        player_1_deck_order = [dominion.COPPER.id] * 4 + [dominion.ESTATE.id] * 1 + [dominion.COPPER.id] * 3 + [dominion.ESTATE.id] * 2
+        for a in player_0_deck_order:
+            state.apply_action(a)
+        for a in player_1_deck_order:
+            state.apply_action(a)
+        
+        actual_actions = []
         while state.current_player() is 0:
             action = bots[0].step(state)
             state.apply_action(action)
-            actions.append(action)
+            actual_actions.append(action)
 
-        expected_actions = [dominion.COPPER.play] * 4 + [dominion.SILVER.buy]
-        np.testing.assert_array_equal(expected_actions,actions)
+        expected_actions = [dominion.COPPER.play] * 4 + [dominion.END_PHASE_ACTION] + [dominion.SILVER.buy] + [dominion.END_PHASE_ACTION]
+        np.testing.assert_array_equal(actual_actions,expected_actions)
 
     def test_buys_silver_with_5_coins(self):
-        game = pyspiel.load_game(FLAGS.game, {"num_players": FLAGS.num_players,"kingdom_cards": FLAGS.kingdom_cards})
+        game = pyspiel.load_game('python_dom',params)
+        observer = game.make_py_observer()
         bots = [
-            dominion_bots.BigMoneyBot(0),
-            dominion_bots.BigMoneyBot(1)
+            dominion_bots.BigMoneyBot(observer=observer,player_id=0),
+            dominion_bots.BigMoneyBot(observer=observer,player_id=1),
         ]
         state = game.new_initial_state()
-        #mock hand
-        state.load_hand(['Copper','Copper','Copper','Copper','Copper'])
-        actions = []
+        player_0_deck_order = [dominion.COPPER.id] * 7 + [dominion.ESTATE.id] * 3
+        player_1_deck_order = [dominion.COPPER.id] * 7 + [dominion.ESTATE.id] * 3
+        for a in player_0_deck_order:
+            state.apply_action(a)
+        for a in player_1_deck_order:
+            state.apply_action(a)
+        
+        actual_actions = []
         while state.current_player() is 0:
             action = bots[0].step(state)
             state.apply_action(action)
-            actions.append(action)
+            actual_actions.append(action)
 
-        expected_actions = [dominion.COPPER.play] * 5 + [dominion.SILVER.buy]
-        np.testing.assert_array_equal(expected_actions,actions)
+
+        expected_actions = [dominion.COPPER.play] * 5 + [dominion.END_PHASE_ACTION,dominion.SILVER.buy,dominion.END_PHASE_ACTION]
+        np.testing.assert_array_equal(expected_actions,actual_actions)
     
-    def test_buys_gold_with_6_coins(self):
-        game = pyspiel.load_game(FLAGS.game, {"num_players": FLAGS.num_players,"kingdom_cards": FLAGS.kingdom_cards})
+    def test_buy_gold_with_6_coins(self):
+        game = pyspiel.load_game('python_dom',params)
+        observer = game.make_py_observer()
         bots = [
-            dominion_bots.BigMoneyBot(0),
-            dominion_bots.BigMoneyBot(1)
+            dominion_bots.BigMoneyBot(observer=observer,player_id=0),
+            dominion_bots.BigMoneyBot(observer=observer,player_id=1),
         ]
         state = game.new_initial_state()
-        #mock draw_pile and hand
-        state.get_current_player().draw_pile += [dominion.SILVER]
-        state.load_hand(['Copper','Copper','Copper','Copper','Silver'])
-        actions = []
+        player_0_deck_order = [dominion.COPPER.id] * 7 + [dominion.ESTATE.id] * 3
+        player_1_deck_order = [dominion.COPPER.id] * 7 + [dominion.ESTATE.id] * 3
+        for a in player_0_deck_order:
+            state.apply_action(a)
+        for a in player_1_deck_order:
+            state.apply_action(a)
+        
+        current_player_state(state).draw_pile.append(dominion.SILVER)
+        
+        load_hand(state,['Silver','Copper','Copper','Copper','Copper'])
+        
+        actual_actions = []
         while state.current_player() is 0:
             action = bots[0].step(state)
             state.apply_action(action)
-            actions.append(action)
-
-        expected_actions = [dominion.COPPER.play] * 4 + [dominion.SILVER.play] + [dominion.GOLD.buy]
-        np.testing.assert_array_equal(expected_actions,actions)
+            actual_actions.append(action)
+        expected_actions = [dominion.COPPER.play] * 4 + [dominion.SILVER.play,dominion.END_PHASE_ACTION,dominion.GOLD.buy,dominion.END_PHASE_ACTION] 
+        np.testing.assert_array_equal(expected_actions,actual_actions)
 
     def test_buys_gold_with_7_coins(self):
-        game = pyspiel.load_game(FLAGS.game, {"num_players": FLAGS.num_players,"kingdom_cards": FLAGS.kingdom_cards})
+        game = pyspiel.load_game('python_dom',params)
+        observer = game.make_py_observer()
         bots = [
-            dominion_bots.BigMoneyBot(0),
-            dominion_bots.BigMoneyBot(1)
+            dominion_bots.BigMoneyBot(observer=observer,player_id=0),
+            dominion_bots.BigMoneyBot(observer=observer,player_id=1),
         ]
         state = game.new_initial_state()
-        #mock draw_pile and hand
-        state.get_current_player().draw_pile += [dominion.SILVER] * 2
-        state.load_hand(['Copper','Copper','Copper','Silver','Silver'])
-        actions = []
+        player_0_deck_order = [dominion.COPPER.id] * 7 + [dominion.ESTATE.id] * 3
+        player_1_deck_order = [dominion.COPPER.id] * 7 + [dominion.ESTATE.id] * 3
+        for a in player_0_deck_order:
+            state.apply_action(a)
+        for a in player_1_deck_order:
+            state.apply_action(a)
+    
+        current_player_state(state).draw_pile += [dominion.SILVER] * 2
+        load_hand(state,['Copper','Copper','Copper','Silver','Silver'])
+        actual_actions = []
         while state.current_player() is 0:
             action = bots[0].step(state)
             state.apply_action(action)
-            actions.append(action)
+            actual_actions.append(action)
 
-        expected_actions = [dominion.COPPER.play] * 3 + [dominion.SILVER.play] * 2 + [dominion.GOLD.buy]
-        np.testing.assert_array_equal(expected_actions,actions)
+        expected_actions = [dominion.COPPER.play] * 3 + [dominion.SILVER.play] * 2 + [dominion.END_PHASE_ACTION,dominion.GOLD.buy,dominion.END_PHASE_ACTION]
+        np.testing.assert_array_equal(expected_actions,actual_actions)
 
     def test_buys_province_with_8_coins(self):
-        game = pyspiel.load_game(FLAGS.game, {"num_players": FLAGS.num_players,"kingdom_cards": FLAGS.kingdom_cards})
+        game = pyspiel.load_game('python_dom',params)
+        observer = game.make_py_observer()
         bots = [
-            dominion_bots.BigMoneyBot(0),
-            dominion_bots.BigMoneyBot(1)
+            dominion_bots.BigMoneyBot(observer=observer,player_id=0),
+            dominion_bots.BigMoneyBot(observer=observer,player_id=1),
         ]
         state = game.new_initial_state()
-        #mock draw_pile and hand
-        state.get_current_player().draw_pile += [dominion.GOLD] * 2
-        state.load_hand(['Copper','Copper','Estate','Gold','Gold'])
-        actions = []
-        while state.current_player() is 0:
-            action = bots[0].step(state)
-            state.apply_action(action)
-            actions.append(action)
-
-        expected_actions = [dominion.COPPER.play] * 2 + [dominion.GOLD.play] * 2 + [dominion.PROVINCE.buy]
-        np.testing.assert_array_equal(expected_actions,actions)
+        player_0_deck_order = [dominion.COPPER.id] * 7 + [dominion.ESTATE.id] * 3
+        player_1_deck_order = [dominion.COPPER.id] * 7 + [dominion.ESTATE.id] * 3
+        for a in player_0_deck_order:
+            state.apply_action(a)
+        for a in player_1_deck_order:
+            state.apply_action(a)
     
-    def test_treasure_card_not_avail_for_purchase(self):
-        game = pyspiel.load_game(FLAGS.game, {"num_players": FLAGS.num_players,"kingdom_cards": FLAGS.kingdom_cards})
-        bots = [
-            dominion_bots.BigMoneyBot(0),
-            dominion_bots.BigMoneyBot(1)
-        ]
-        state = game.new_initial_state()
-        #mock draw_pile and hand
-        state.get_current_player().draw_pile += [dominion.SILVER] * 2
-        state.load_hand(['Copper','Copper','Copper','Silver','Silver'])
-        
-        state.supply_piles[dominion.GOLD.name].qty = 0
-        actions = []
+        current_player_state(state).draw_pile += [dominion.SILVER] * 3
+        load_hand(state,['Copper','Copper','Silver','Silver','Silver'])
+        actual_actions = []
         while state.current_player() is 0:
             action = bots[0].step(state)
             state.apply_action(action)
-            actions.append(action)
+            actual_actions.append(action)
 
-        expected_actions = [dominion.COPPER.play] * 3 + [dominion.SILVER.play] * 2 + [dominion.END_PHASE_ACTION]
-        np.testing.assert_array_equal(expected_actions,actions)
+        expected_actions = [dominion.COPPER.play] * 2 + [dominion.SILVER.play] * 3 + [dominion.END_PHASE_ACTION,dominion.PROVINCE.buy,dominion.END_PHASE_ACTION]
+        np.testing.assert_array_equal(expected_actions,actual_actions)
+    
+    def test_buys_duchy_if_has8coins_duchydancing_penultimate_province(self):
+        game = pyspiel.load_game('python_dom',params)
+        observer = game.make_py_observer()
+        bots = [
+            dominion_bots.BigMoneyBot(observer=observer,player_id=0,duchy_dancing=True),
+            dominion_bots.BigMoneyBot(observer=observer,player_id=1),
+        ]
+        state = game.new_initial_state()
+        player_0_deck_order = [dominion.COPPER.id] * 7 + [dominion.ESTATE.id] * 3
+        player_1_deck_order = [dominion.COPPER.id] * 7 + [dominion.ESTATE.id] * 3
+        for a in player_0_deck_order:
+            state.apply_action(a)
+        for a in player_1_deck_order:
+            state.apply_action(a)
+    
+        current_player_state(state).draw_pile += [dominion.SILVER] * 3
+        state.supply_piles[dominion.PROVINCE.name].qty = 1
+        load_hand(state,['Copper','Copper','Silver','Silver','Silver'])
+        actual_actions = []
+        while state.current_player() is 0:
+            action = bots[0].step(state)
+            state.apply_action(action)
+            actual_actions.append(action)
 
+        expected_actions = [dominion.COPPER.play] * 2 + [dominion.SILVER.play] * 3 + [dominion.END_PHASE_ACTION,dominion.DUCHY.buy,dominion.END_PHASE_ACTION]
+        np.testing.assert_array_equal(expected_actions,actual_actions)
 if __name__ == "__main__":
     absltest.main()
 
